b653d70b510cee374f25b70645f70b0f2e0e9602
lcunn <cunn2001@gmail.com>
2024-07-04 17:45:33 +0100

    working melody extractor

--- src/embeddings/music_vae/encoder.py
+++ src/embeddings/music_vae/encoder.py
@@ -1,7 +1,10 @@
-from typing import str
+from typing import Dict, List, Optional
 
 from src.midi.midi_processor import MidiProcessor
 
+from src.embeddings.music_vae.split_ns import split_into_2_bar_segments, extract_2_bar_melodies
+
+# from note_seq.protobuf.music_pb2 import NoteSequence
 from note_seq.sequences_lib import extract_subsequence
 from note_seq.midi_io import midi_file_to_note_sequence, note_sequence_to_midi_file
 from magenta.music import sequences_lib
@@ -14,64 +17,46 @@ Using the encoder from the notebook...
 
 """
 
-
 class MusicVAEncoder(MidiProcessor):
-    def __init__(self, file_path: str, args):
+
+    def __init__(self, file_path: str):
         super().__init__(file_path)
         self._note_sequence = midi_file_to_note_sequence(file_path)
-        self.args = args  #what are these args?
+        self._melodies = None
 
-
-    def _calculate_two_bar_duration(self):
-        if self._note_sequence.tempos:
-            bpm = self._note_sequence.tempos[0].qpm  # Using the first tempo for calculation
-        else:
-            bpm = 120  # Default tempo
-        
-        if self._note_sequence.time_signatures:
-            beats_per_bar = self._note_sequence.time_signatures[0].numerator  # Assuming simple time signatures
-        else:
-            beats_per_bar = 4  # Common time
-        
-        seconds_per_beat = 60 / bpm
-        return 2 * beats_per_bar * seconds_per_beat  # Duration of two bars
-    
-    def split_into_2_bar_segments(midi, quantize=True, steps_per_quarter=8):
+    def extract_melodies(
+        self,
+        quantize_steps_per_quarter: int = 8
+    ) -> None:
         """
-        Takes a midi file and splits it into a list of 2-bar NoteSequence objects with a window shift of 1 bar.
+        Extracts all melodies from the list of 2 bar segments using the magenta MelodyExtractor and stores in instance.
 
         Args:
-            midi: path to the midi file.
-            quantize: binary, whether to quantize or not.
-            steps_per_quarter: argument for the Quantizer instance.
-
-        Returns:
-            List of NoteSequence objects.
+            quantize_steps_per_quarter: whether to quantize the melody.
         """
-        ns = midi_file_to_note_sequence(midi)
 
-        quantizer = Quantizer(steps_per_quarter=8)
-
-        # Calculate the duration of two bars
-        hop_size_seconds = calculate_two_bar_duration(ns)
-
-        # Split the NoteSequence into segments of two bars each
-        splitter = Splitter(hop_size_seconds=hop_size_seconds)
-        two_bar_segments = splitter.transform(ns)
+        two_bar_segments = split_into_2_bar_segments(
+            self._note_sequence, 
+            quantize = True, 
+            quantize_steps_per_quarter = quantize_steps_per_quarter)
         
-        # Get the 2 bars shifted 1 bar forwards
-        shifted_ns = extract_subsequence(ns, hop_size_seconds/2, ns.total_time)
-        shifted_two_bar_segments = splitter.transform(shifted_ns)
-        
-        two_bar_segments = two_bar_segments + shifted_two_bar_segments
+        melodies = {}
 
-        if quantize:
-            # loop over and quantize each segment
-            two_bar_segments_quant = two_bar_segments
+        for start_bar, segment in enumerate(two_bar_segments):
+            mels = extract_2_bar_melodies(segment)
+            if mels:
+                melodies[str(start_bar)] = mels
+            else:
+                melodies[str(start_bar)] = None
 
-            for i, segment in enumerate(two_bar_segments):
-                two_bar_segments_quant[i] = quantizer.transform(segment)[0]
-            return two_bar_segments_quant
+        self._melodies = melodies
+
+        @property
+        def melodies(self):
+            return self._melodies
+            
         
-        else:
-            return two_bar_segments
+
+
+
+        
--- src/midi/midi_processor.py
+++ src/midi/midi_processor.py
@@ -1,4 +1,4 @@
-from typing import str
+# from typing import 
 
 import mido
 
