2e4b5d44805a9a67cb7c77b19784d0092f83539c
Leo <101711950+lcunn@users.noreply.github.com>
2024-08-15 12:56:53 +0000

    update

--- scripts/csvs/helper/process_cases_with_ai.py
+++ scripts/csvs/helper/process_cases_with_ai.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import pandas as pd
 
 from src.defaults import *
+from scripts.csvs.helper.ai_prompts import *
 
 client = OpenAI()
 
@@ -10,40 +11,6 @@ class ai_output(BaseModel):
     artist: str
     title: str
     confidence: float
-
-SYSTEM_PROMPT = """
-You are an expert at music copyright history. 
-We have a list of songs that have appeared in music copyright cases. 
-The end goal is, starting from the court description of a music copyright case, as well as some extra information, return the commonly known artists and titles of the songs involved, and which songs they are compared to in the case.
-This is because we then want to search for these songs in other databases, and obtain official IDs using the MusicBrainz API.
-You will receive the following 8 fields of information:
-- year: the year of the copyright case
-- case_name: the name of the copyright case
-- court: the court in which the
-- complaining_work: the court-given description of the complaining work 
-- defending_work: the court-given description of the defending work
-- complaining_author: the court-given name of the complaining author
-- defending: the court-given name of the defender
-- additional information: more information about the case; this could be comments/opinions/summaries
-Note that:
-- complaining and defending work will mostly be descriptors of individual songs, but can also contain reference several songs
-- complaining author and defending can be the stage names of artists, or government names, and can also include lawyers. You will have to correctly infer the stage name of the artist.
-Your job is, using analytical thinking, the all the fields and the additional information, and your own deep knowledge of copyright history, to extract the title and artist strings that will be likely to come up in the MusicBrainz database. 
-You will also give a confidence, between 0 and 1, of how likely you think it is that you're right.
-
-Example:
-
-Prompt:
-case_year: 2019
-case_name: Batiste v. Lewis, et al.
-complaining/defending_work: Thrift Shop; Neon Cathedral
-complaining_author/defending: Ryan Lewis; Ben Haggerty ("Macklemore")
-
-Output:
-artist: Macklemore & Ryan Lewis feat. Wanz
-title: Thrift Shop
-confidence: 0.99
-"""
 
 def _format_user_prompt(year: str, name: str, work: str, author: str) -> str:
     prompt = f"""
@@ -60,10 +27,10 @@ def process_single_case(year: str, name: str, work: str, author: str):
     completion = client.beta.chat.completions.parse(
         model="gpt-4o-2024-08-06",
         messages=[
-            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "system", "content": GPT_SYSTEM_PROMPT},
             {"role": "user", "content": user_prompt},
         ],
-        response_format=MBOutput,
+        response_format=ai_output,
     )
 
     event = completion.choices[0].message.parsed
